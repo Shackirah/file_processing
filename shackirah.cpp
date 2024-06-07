bec8f1292433b9f5b3a539955114f955f79ad40a
Shackirah <smaulidih@gmail.com>
2024-06-07 14:50:10 +0200

    File
    creating a file

--- shackirah.cpp
+++ shackirah.cpp
@@ -1,46 +1,51 @@
 #include <iostream>
 #include <fstream>
+
 using namespace std;
-void createFile ();
+string createFile ();
 void readFile ();
 void writeFile ();
 
 
 int main()
 {
-    //cout << "hello world";
     int option;
-    while(1)
-    {
+   // while(1)
+    //{
        
         cout <<"Choose option\n" <<"1. Write to a file\n" <<"2. Read from a file\n"
              <<"3. Create a file\n" <<"4. Exit"<<endl;
         cin  >>option;
-        /* switch (option)
+        switch (option)
         {
          case 1:
-             writeFile();
+             //writeFile();
              break;
         case 2:
-             readFile();
+             //readFile();
              break;
         case 3:
              createFile();
              break;
-        case 4:
-             break;
+        //case 4:
+            // break;
         default:
             cout <<"Oooops!!!!!!!!!! invalid input"<<endl;
-        }*/
-    }
+        }
+   // }
     
 
-return 0;
+//return 0;
 }
-void createFile ()
+string createFile ()
 {
-    ofstream myFile("text.txt");
-
+    string fileName;
+    string extention =".txt";
+    cout <<"Enter a file name to be created"<<endl;
+    cin >> fileName;
+    fileName = fileName + extention;
+    ofstream myFile(fileName);
+    return fileName;
 }
 void readFile ()
 {
